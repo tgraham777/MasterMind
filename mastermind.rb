20e1aaaaaf1103a29e16c1db8733aa0442c55bca
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-02-25 11:05:36 -0700

    adds all functions except randomizer and timer

--- mastermind.rb
+++ mastermind.rb
@@ -1,24 +1,47 @@
 require_relative 'response'
 
 class Mastermind
+
+  #make secret random
+  #add timer
+
+  def initialize
+    @guess_count = 0
+  end
+
   def execute(input)
     secret = "bgry"
+
     if input == "q"
-      Response.new(:message => "Thanks for playing!", :status => :won)
+      Response.new(:message => "Thanks for playing! You made #{@guess_count} guesses.", :status => :won)
     elsif input == "c"
-      puts "The combination was #{secret}"
-      Response.new(:message => "Thanks for playing!", :status => :won)
+      Response.new(:message => "The combination was #{secret}. You made #{@guess_count} guesses. Come back and try again!", :status => :won)
     else
-      if input.length == 4
-        if input == secret
-          Response.new(:message => "You win!", :status => :won)
-        else
-          Response.new(:message => "Guess again!", :status => :continue)
-          # this is where it should put feedback on how
-          # many colors/positions are correct
-        end
+      if input.length > 4
+        Response.new(:message => "Error - input is too long or includes unacceptable characters. Please enter 4 letters (r, g, b, or y) without spaces")
+      elsif input.length < 4
+        Response.new(:message => "Error - input is too short or includes unacceptable characters. Please enter 4 letters (r, g, b, or y) without spaces")
       else
-        Response.new(:message => "Error: please enter 4 letters (r, g, b, or y) without spaces")
+        @guess_count += 1
+        if input == secret
+          Response.new(:message => "Congratulations! You guessed the secret combination #{secret} in #{@guess_count} guesses!", :status => :won)
+        else
+          color_count = 0
+          secret.split("").map do |x|
+            if input.include?(x)
+              color_count += 1
+            end
+          end
+
+          position_count = 0
+          secret.split("").each_with_index.map do |x,i|
+            if input[i] == secret[i]
+              position_count += 1
+            end
+          end
+          Response.new(:message => "#{input} has #{color_count} of the correct elements with #{position_count} in the correct positions.
+You have made #{@guess_count} guesses. Guess again!", :status => :continue)
+        end
       end
     end
   end
