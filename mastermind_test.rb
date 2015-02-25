c2a60a16ba1ab75bf8094ac7cf2d5481419e31b0
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-02-25 11:06:04 -0700

    adds tests for all functions except randomizer and timer

--- mastermind_test.rb
+++ mastermind_test.rb
@@ -3,29 +3,33 @@ require 'minitest/pride'
 require_relative 'mastermind'
 
 class MastermindTest < Minitest::Test
-  def test_it_exists
-    assert MastermindTest
-  end
 
   def test_it_wins
     mm = Mastermind.new
     input = "BGRY"
     result = mm.execute(input.downcase)
-    assert result.message.downcase.include?("win")
+    assert result.message.downcase.include?("congratulations")
   end
 
   def test_it_quits
     mm = Mastermind.new
     input = "Q"
     result = mm.execute(input.downcase)
-    assert_equal result.message, "Thanks for playing!"
+    assert result.message.include?("Thanks for playing!")
   end
 
-  def test_it_tests_length
+  def test_it_tests_too_short
     mm = Mastermind.new
     input = "bgr"
     result = mm.execute(input.downcase)
-    assert result.message.downcase.include?("error")
+    assert result.message.downcase.include?("too short")
+  end
+
+  def test_it_tests_too_long
+    mm = Mastermind.new
+    input = "bgrry"
+    result = mm.execute(input.downcase)
+    assert result.message.downcase.include?("too long")
   end
 
   def test_response_to_bad_guess
@@ -47,7 +51,52 @@ class MastermindTest < Minitest::Test
     mm = Mastermind.new
     input = "C"
     result = mm.execute(input.downcase)
-    assert_equal result.message, "Come back and try again!"
+    assert result.message.include?("Come back and try again!")
+  end
+
+  def test_color_count
+    mm = Mastermind.new
+    input = "ggry"
+    result = mm.execute(input.downcase)
+    assert result.message.include?("3 of the correct elements")
+  end
+
+  def test_position_count
+    mm = Mastermind.new
+    input = "rgby"
+    result = mm.execute(input.downcase)
+    assert result.message.include?("2 in the correct positions")
+  end
+
+  def test_guess_count
+    mm = Mastermind.new
+    input = "rgby"
+    result = mm.execute(input.downcase)
+    assert result.message.include?("1 guesses")
+  end
+
+  def test_guess_count_multiple
+    mm = Mastermind.new
+    input = "rgby"
+    mm.execute(input.downcase)
+    input = "rrby"
+    mm.execute(input.downcase)
+    input = "ggrb"
+    result = mm.execute(input.downcase)
+    assert result.message.include?("3 guesses")
+  end
+
+  def test_it_does_not_count_mistakes
+    mm = Mastermind.new
+    input = "rgby"
+    mm.execute(input.downcase)
+    input = "rgbyg"
+    mm.execute(input.downcase)
+    input = "rrby"
+    mm.execute(input.downcase)
+    input = "ggrb"
+    result = mm.execute(input.downcase)
+    assert result.message.include?("3 guesses")
   end
 
 end
