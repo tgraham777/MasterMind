4ff87358a33a2c1dd5a56f452781e06ae4a30b01
Tyler Graham <Tyler@Tylers-MBP.Home>
2015-02-24 22:14:00 -0700

    adds basic mastermind functions

---
+++ mastermind.rb
@@ -0,0 +1,22 @@
+require_relative 'response'
+
+class Mastermind
+  def execute(input)
+    secret = "bgry"
+    if input == "q"
+      Response.new(:message => "Thanks for playing!", :status => :won)
+    else
+      if input.length == 4
+        if input == secret
+          Response.new(:message => "You win!", :status => :won)
+        else
+          Response.new(:message => "Guess again!", :status => :continue)
+          # this is where it should put feedback on how
+          # many colors/positions are correct
+        end
+      else
+        Response.new(:message => "Error: please enter 4 letters (r, g, b, or y) without spaces")
+      end
+    end
+  end
+end
